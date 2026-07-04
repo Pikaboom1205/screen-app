acf392d6064886b4e95f41b658dc2505ac4e101e
Chen Guanzi <jcg120512@gmail.com>
2026-07-04 10:06:22 +0800

    Initial Commit

---
+++ ScreenApp/ScreenAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenAppApp.swift
+//  ScreenApp
+//
+//  Created by JC on 4/7/26.
+//
+
+import SwiftUI
+
+@main
+struct ScreenAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
